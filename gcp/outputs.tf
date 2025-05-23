c464ff3e323e0e893a22017b4cbd2143f5d09eb8
MarckMorris <127154744+MarckMorris@users.noreply.github.com>
2025-05-23 13:01:13 -0500

    Update outputs.tf

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,3 +1,7 @@
-git remote add origin https://github.com/TU_USUARIO/multicloud-terraform.git
-git branch -M main
-git push -u origin main
+output "instance_name" {
+  value = google_compute_instance.vm_instance.name
+}
+
+output "instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
